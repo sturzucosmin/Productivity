da3204eb1d85d148229fd0e415a37628b9e8831e
Bogdan Piele <bogdanpielemail@gmail.com>
2021-11-07 15:05:20 +0200

    First commit
    Created mock for time set up page

---
+++ entry/src/main/java/com/example/productivity/slice/MainAbilitySlice.java
@@ -0,0 +1,36 @@
+package com.example.productivity.slice;
+
+import com.example.productivity.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.TimePicker;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+
+        TimePicker timePicker = (TimePicker) findComponentById(ResourceTable.Id_time_picker);
+        timePicker.showSecond(false);
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+        timePicker.setHour(20);
+        timePicker.setMinute(10);
+
+        timePicker.setTimeChangedListener((timePicker1, hour1, minute1, second1) -> {
+            //ToDo:
+        });
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
